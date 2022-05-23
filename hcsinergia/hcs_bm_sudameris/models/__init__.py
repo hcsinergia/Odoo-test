f1b5706cbe10633a6de8b1a517b91afedda29d66
Facundo Perez <facundoperez94@gmail.com>
2022-05-23 10:53:17 -0300

    init

---
+++ hcsinergia/hcs_bm_sudameris/models/__init__.py
@@ -0,0 +1,2 @@
+from . import res_country, res_company, res_user, bm_branch, bm_official, bm_product, bm_department, bm_job, bm_official_category, bm_official_salary, bm_official_departure
+from . import bm_official_api, API
